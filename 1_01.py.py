850969404a62991eb5cef404fab6518c71eed6ee
YuMin Kim <helloaiworld@gmail.com>
2022-07-04 22:57:00 +0900

    1_01 Completion

---
+++ 1_01.py.py
@@ -0,0 +1,8 @@
+col, row = map(int, input().split(' ')) #input numbers and seperate with spaces 
+
+for r in range(row): # repeat row 
+    for c in range(col): #repeat column
+        print("*", end = '') 
+    print() #move on the next line
+
+    
