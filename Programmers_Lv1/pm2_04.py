9f376ea6440a239a26050f05456b3a48fd73d47f
Dayoming <jdayoung1204@naver.com>
2022-07-14 10:26:34 +0900

    자연수 뒤집어 배열로 만들기 업로드

---
+++ Programmers_Lv1/pm2_04.py
@@ -0,0 +1,8 @@
+def solution(n):
+    temp = str(n)  # Replace natural numbers with characters
+    answer = []
+    for i in temp[::-1]:  # List reverse
+        answer.append(int(i))
+    return answer
+
+print(solution(12345))
