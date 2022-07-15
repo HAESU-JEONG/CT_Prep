b4d9c43d55cc02e6548572282d38b56b6d1ba8a8
YeonJang <lej29331814@gmail.com>
2022-07-15 22:06:57 +0900

    Lv.1

---
+++ Programmers_Lv.1/pm2_05.py
@@ -0,0 +1,11 @@
+def solution(n): # solution defined
+    answer = 0 # initialize answer 0
+    n = str(n) # Replace numbers with strings
+    
+    for i in (n): # repeat until n
+        answer += int(i) # answer = answer + ini(i)
+    
+    return answer # return answer
+
+n = int(input()) # input number
+print(solution(n)) # print solution(n)
