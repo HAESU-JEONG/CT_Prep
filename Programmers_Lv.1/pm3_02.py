e0913aa2edf314535f084f0ac9504ad8b05832c3
YeonJang <lej29331814@gmail.com>
2022-07-19 23:25:50 +0900

    Lv.1

---
+++ Programmers_Lv.1/pm3_02.py
@@ -0,0 +1,6 @@
+def solution(n): # defined solution function
+    answer = 0 # initialize answer 0
+    for i in range(1, n + 1): # repeat until i in range 1, n + 1
+        if (n % i == 0): # if i is the measure(mineral number) of n
+            answer += i # answer = answer + i
+    return answer # return answer
