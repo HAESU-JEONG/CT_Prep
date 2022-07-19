12f16f0d74a7a79891d8e749aa51b33419d8adf2
YeonJang <lej29331814@gmail.com>
2022-07-19 22:29:30 +0900

    Lv.1

--- Programmers_Lv.1/pm3_01.py
+++ Programmers_Lv.1/pm3_01.py
@@ -1,11 +1,11 @@
-def solution(s): 
-    answer = ''
-    s = s.split(" ")
-    for i in (s):
-        for j in range(len(i)):
-            if j % 2 == 0:
-                answer += i[j].upper()
+def solution(s): # solution function defined
+    answer = '' # initialize answer
+    s = s.split(" ") # split by space
+    for i in (s): # repeat until s
+        for j in range(len(i)): # repeat until length of i
+            if j % 2 == 0: # if j is even number
+                answer += i[j].upper() # replace uppercase letters
             else:
-                answer += i[j].lower()
-        answer += ' '
-    return answer[:-1]
+                answer += i[j].lower() # replace lowercase letters
+        answer += ' ' # add spaces
+    return answer[:-1] # slice end spaces
