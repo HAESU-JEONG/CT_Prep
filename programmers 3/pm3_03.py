6a0d0fd1dd2e5faee0f86a370f513f9a873a4678
unknown <helloaiworld01@gmail.com>
2022-07-20 10:34:18 +0900

    completion 3_03 with restriction

--- programmers 3/pm3_03.py
+++ programmers 3/pm3_03.py
@@ -1,6 +1,20 @@
+'''
 def solution(str):
     num = int(str) #convert str to int
     return num 
 
 str = input()
 print(solution(str))
+'''
+def solution(str):
+    num = int(str) #convert str to int
+    return num 
+
+str = input()
+
+while(len(str) >= 5 or len(str) < 1): #Restriction 1. str's length 
+    if str[0] != 0 : #Restriction2. the beginning of str isn't 0
+        str = input() 
+
+print(solution(str))
+
