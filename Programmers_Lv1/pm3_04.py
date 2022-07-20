eeb680e33c5b12de68961e1f0d6d729abad6699a
Dayoming <jdayoung1204@naver.com>
2022-07-21 00:46:03 +0900

    주석 추가

--- Programmers_Lv1/pm3_04.py
+++ Programmers_Lv1/pm3_04.py
@@ -1,10 +1,10 @@
 def solution(n):
-    answer = ''
+    answer = ''  # Variables to store results
     for i in range(1, n + 1):
-        if i % 2 != 0:
-            answer += '수'
-        else:
-            answer += '박'
+        if i % 2 != 0:  # If i is odd
+            answer += '수'  # answer add '수'
+        else:  # If i is even
+            answer += '박'  # answer add '박'
     return answer
 
 
