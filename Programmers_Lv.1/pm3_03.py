452bb357f2c2cbfaf57a907ecc2afd991a02902c
YeonJang <lej29331814@gmail.com>
2022-07-20 23:55:24 +0900

    Lv.1

---
+++ Programmers_Lv.1/pm3_03.py
@@ -0,0 +1,6 @@
+def solution(s): # solution function defined
+    answer = int(s) # change string value to int value
+    return answer # return answer
+
+s = input() # input string value
+print(solution(s)) # print solution(s)
