814a7347d2ca322eb9026880abebb537b6299d50
Dayoming <jdayoung1204@naver.com>
2022-08-11 17:37:41 +0900

    없는 숫자 더하기 업로드

---
+++ Programmers_Lv1/pm6_04.py
@@ -0,0 +1,13 @@
+def solution(numbers):
+    # Initialize variable result
+    result = 0
+    # 0~9 integer array
+    range_numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    # If range_numbers not in number then result + i
+    for i in range_numbers:
+        if i not in numbers:
+            result += i
+    return result
+
+
+print(solution([5, 8, 4, 0, 6, 7, 9]))
