2d05ec3a49a46df315050972755fa3c9951bd165
YuMin Kim <helloaiworld@gmail.com>
2022-08-13 01:30:42 +0900

    1st fault

--- programmers 6/pm6_05.py
+++ programmers 6/pm6_05.py
@@ -2,10 +2,11 @@ def solution(ori):
     number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] 
     number_s = ['zero','one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
     
-    for i in len(number):
-        
-
-        
+    for i in range(len(number)):
+        if ori.find('number_s[i]'):
+            ori.strip('number_s[i]')
+            
+    return ori
 
 ori = input()
 print(solution(ori))
