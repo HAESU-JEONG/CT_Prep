bd54ad2ea3e5e65f088219cffca4fa425b998da5
YuMin Kim <helloaiworld@gmail.com>
2022-08-13 00:52:59 +0900

    6_05 리스트 써보기

---
+++ programmers 6/pm6_05.py
@@ -0,0 +1,11 @@
+def solution(ori):
+    number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] 
+    number_s = ['zero','one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    
+    for i in len(number):
+        
+
+        
+
+ori = input()
+print(solution(ori))
