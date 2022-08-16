ab773aba459ed902a8692e1c0d1f7794e430336e
YuMin Kim <helloaiworld@gmail.com>
2022-08-16 15:40:01 +0900

    7_02 미완

---
+++ programmers 7/pm7_02.py
@@ -0,0 +1,13 @@
+id_list = ["muzi", "frodo", "apeach", "neo"]
+report = ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"]
+k = 2
+
+def solution(id_list, report, k):
+    answer = []
+    report = list(set(report)) #중복 제거 = 한 사람이 같은 사람을 여러번 신고한 경우 제거
+
+    
+
+    
+
+solution(id_list, report, k)
