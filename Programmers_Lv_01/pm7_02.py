2c71a410a9ea49e0be5086a28a423a1e5c862084
HAESU-JEONG <junghaesu7@gmail.com>
2022-08-16 22:19:46 +0900

    신고 결과 받기

---
+++ Programmers_Lv_01/pm7_02.py
@@ -0,0 +1,25 @@
+def solution(id_list, report, k):
+    answer = [0]  * len(id_list)
+    # make list to return
+    # 'answer' list's purpose : save number of mail
+
+    reports = {x : 0 for x in id_list} 
+    # make dictionary
+    # dictionary's key => name in id_list
+    # dictionary's value => initialize to zero
+    # 'reports' dictionary is to check how many times the person have been reported
+
+    report = set(report) # remove duplicate values
+
+    for record in report:
+        reported_person = record.split()[1] # save the second of the two names
+        reports[reported_person] += 1 # add 1 to the value with 'reported_person' as an index
+
+    for record in report:
+        reported_person = record.split()[1]
+        if reports[reported_person] >= k: # if the value is greater than the 'k'
+            answer[id_list.index(reported_person)] += 1
+            # id_list.index(reported_person) => find index reported_person in id_list
+            # add 1 to the answer's value 
+    
+    return answer
