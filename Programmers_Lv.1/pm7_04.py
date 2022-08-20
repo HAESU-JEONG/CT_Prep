f1dffd9d6e27fa5847906c7536bc9116c5e43b84
YeonjangL <lej29331814@gmail.com>
2022-08-21 01:48:11 +0900

    Lv.2

--- Programmers_Lv.1/pm7_04.py
+++ Programmers_Lv.1/pm7_04.py
@@ -1,14 +1,11 @@
-def solution(number, k):
-    answer = []
-    for i in number:
-        while answer and answer[-1] < i and k > 0:
-            answer.pop()
-            k -= 1
-        answer.append(i)
-    if k > 0:
-        answer = answer[:-k]
+def solution(number, k): # solution function defined
+    answer = [] # initializae answer stack
+    for i in number: # repeat until i in number
+        while answer and answer[-1] < i and k > 0: # repeat until answer and answer[-1] is smaller than i and k is bigger than 0
+            answer.pop() # delete value and obtain deleted value
+            k -= 1 # k = k - 1
+        answer.append(i) # append answer i
+    if k > 0: # if k is bigger than 0
+        answer = answer[:-k] # delete value has not been deleted
     
-    return ''.join(answer)
-
-number, k = input().split() # input number
-print(solution(number, k))
+    return ''.join(answer) # return answer
