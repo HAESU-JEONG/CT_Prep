07b4ee20c819260e068500ef12199f6f00295fa7
HAESU-JEONG <junghaesu7@gmail.com>
2022-08-23 15:25:28 +0900

    백준 1920 풀이(수정)

--- Baekjoon/BJ1920.py
+++ Baekjoon/BJ1920.py
@@ -1,11 +1,29 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 num_list = list(map(int, input().split()))
+num_list.sort()
 
 m = int(input())
 search_num_list = list(map(int, input().split()))
 
-for i in range(len(search_num_list)):
-    if search_num_list[i] in num_list: # if the number you're looking for is on that list
+def binary_search(target):
+    start = 0
+    end = len(num_list) - 1
+
+    while start <= end:
+        mid = (start + end) // 2
+
+        if num_list[mid] == target:
+            return True
+        elif num_list[mid] < target:
+            start = mid + 1
+        else:
+            end = mid - 1
+
+for i in range(m):
+    if binary_search(search_num_list[i]):
         print(1)
     else:
         print(0)
